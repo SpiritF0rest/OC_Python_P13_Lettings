3a61aeee352828c4619a570fb0cc58bbcc0d8523
Weidenthal Sandra <spiritf0rest@hotmail.com>
2023-12-07 19:21:12 +0100

    test: adding unit tests

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,14 @@
-def test_dummy():
-    assert 1
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class TestLettingsSiteIndexView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, "Welcome to Holiday Homes")
