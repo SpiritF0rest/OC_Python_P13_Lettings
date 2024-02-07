7469a70debd2b175bc6ae1ed03d068f16a527e08
Weidenthal Sandra <spiritf0rest@hotmail.com>
2024-02-07 19:06:28 +0100

    wip: config for docker image to production

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.11-bookworm as builder
 
-RUN pip install poetry==1.6.1
+RUN pip install poetry==1.7.1
 
 ENV POETRY_NO_INTERACTION=1 \
     POETRY_VIRTUALENVS_IN_PROJECT=1 \
@@ -15,15 +15,15 @@ RUN --mount=type=cache,target=$POETRY_CACHE_DIR poetry install --only main
 
 FROM python:3.11-slim-bookworm as runtime
 
+WORKDIR /app
+
 ENV VIRTUAL_ENV=/app/.venv \
     PATH="/app/.venv/bin:$PATH"
 
-WORKDIR /app
-
 COPY --from=builder ${VIRTUAL_ENV} ${VIRTUAL_ENV}
 
-COPY . .
+COPY . /app
 
-EXPOSE 8080
+EXPOSE 8000
 
-CMD ["python3", "manage.py", "runserver"]
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,9 +21,9 @@ django_key = os.getenv("SECRET_KEY")
 SECRET_KEY = django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 # Logging configuration
 
